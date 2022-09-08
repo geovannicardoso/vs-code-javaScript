c396b7a856fb29932a325af4a89b843a4398c90c
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-09-08 17:12:24 -0300

    variable-javascript

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+
+// promot-sync
+// npm install prompt-sync
+const prompt = require('prompt-sync')();
+
+const userName = prompt('type your name: ');
+
+console.log(`the typed name was: ${userName}`);
+
+
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 alert('hello world')
 document.write('hello world')
 console.log('hello world')
+
+var diogo4 = 7
+tropaDoMantem = 7
